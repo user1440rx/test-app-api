b6cb331f76671853c0ac595b6fdfad0c0a9a7254
vshnv <109591279+user1440rx@users.noreply.github.com>
2022-12-29 07:36:53 +0530

    Update cors-middleware.js

--- middlewares/cors-middleware.js
+++ middlewares/cors-middleware.js
@@ -1,9 +1,9 @@
 const cors = require('cors');
 
-var allowlist = ['http://localhost:3000', 'https://vercel.app', 'https://test-app-nu-one.vercel.app']
+var allowlist = ['*', 'http://localhost:3000', 'https://vercel.app', 'https://test-app-nu-one.vercel.app']
 var corsOptions = {
     credentials: true,
-    origin: allowlist,
+    origin: 'allowlist',
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
